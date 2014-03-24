b031f23fb46374f97eeae965b91b6c36facc6253
enderceylan <endcey95@gmail.com>
2014-03-23 21:09:55 -0500

    Create HW9-Memoizer.java

---
+++ HW9-Memoizer.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+//interface Functor { Object fn(Object x); }
+public class Memoizer
+{
+	Functor f;
+	HashMap<Object, Object> map;
+
+	public Memoizer(Functor f1)
+	{
+		f = f1;
+		map = new HashMap<Object, Object>();
+	}
+	public Object call(Object x)
+	{
+		if (map.containsKey(x))
+		{
+			return map.get(x);
+		}
+		else
+		{
+			Object y = f.fn(x);
+			map.put(x,y);
+			return y;
+		}
+	}
+}
